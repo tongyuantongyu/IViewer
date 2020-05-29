828c8c86c3e451dd435ad24a90ebbbb82694d101
TYTY <tongyuantongyu@gmail.com>
2020-05-29 11:16:16 +0800

    add explanation to Bitmap class

--- ImageLibrary/Bitmap.cs
+++ ImageLibrary/Bitmap.cs
@@ -16,11 +16,21 @@ namespace ImageLibrary {
   // Also, Bitmap can be passed to native code to do process.
   [StructLayout(LayoutKind.Sequential)]
   public struct Bitmap {
+    // Scan0 represents the begin address of pixel data
     public readonly IntPtr Scan0;
+    // Stride represents length per horizontal line in byte(uint8).
+    // This value can be bigger than size of a valid line (width * channel * bepth / 8),
+    // and in this case following data should not be modified
     public readonly int Stride;
+    // valid area width
     public readonly int Width;
+    // valid area height
     public readonly int Height;
+    // 8 or 16. Also the bit width.
+    // When value is 8, pixel order is bgr(a), and every channel value is in range [0, 255] and type byte(uint8)
+    // When value is 16 pixel order is rgb(a),  and every channel value is in range [0, 65535] and type ushort(uint16)
     public readonly int Depth;
+    // 3 or 4. 4 means alpha present, 3 means absent.
     public readonly int Channel;
 
     public static Bitmap Invalid = new Bitmap(IntPtr.Zero, 0, 0, 0, 0, 0);
